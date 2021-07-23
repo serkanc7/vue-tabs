6ba10c82e466e9bec90ee66769fc0baefed23ac8
Serkan Celik <srknclk98@gmail.com>
2021-07-23 23:57:07 +0300

    svg changed

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,8 +1,11 @@
 <template>
   <footer class="bottom-bar">
     <template v-for="(icon, index) in icons" :key="index">
-      <span @click="$emit('changeTab', index)" class="bottom-bar__icon">
-        <img :src="icon" alt="" />
+      <span
+        @click="$emit('changeTab', index)"
+        class="bottom-bar__icon"
+        v-html="icon"
+      >
       </span>
     </template>
   </footer>
@@ -13,9 +16,51 @@ export default {
   emits: ["changeTab"],
   setup() {
     const icons = [
-      "/src/assets/svg/home.svg",
-      "/src/assets/svg/whatshot.svg",
-      "/src/assets/svg/album.svg",
+      `<svg
+        width="24"
+        height="24"
+        stroke-width="1.5"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3 9.5L12 4L21 9.5"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M19 13V19.4C19 19.7314 18.7314 20 18.4 20H5.6C5.26863 20 5 19.7314 5 19.4V13"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>`,
+      `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M11.57 13.16c-1.36.28-2.17 1.16-2.17 2.41 0 1.34 1.11 2.42 2.49 2.42 2.05 0 3.71-1.66 3.71-3.71 0-1.07-.15-2.12-.46-3.12-.79 1.07-2.2 1.72-3.57 2zM13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM12 20c-3.31 0-6-2.69-6-6 0-1.53.3-3.04.86-4.43 1.01 1.01 2.41 1.63 3.97 1.63 2.66 0 4.75-1.83 5.28-4.43C17.34 8.97 18 11.44 18 14c0 3.31-2.69 6-6 6z"
+        />
+      </svg>`,
+      `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M20 4v12H8V4h12m0-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 9.67l1.69 2.26 2.48-3.1L19 15H9zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z"
+        />
+      </svg>`,
     ];
     return {
       icons,
